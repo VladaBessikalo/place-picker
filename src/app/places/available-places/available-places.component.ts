63c8cdf2296c272ccd68cc2f523be4a5709d9ef6
VladaBessikalo <bess.vlada@gmail.com>
2025-09-18 16:19:03 +0200

    feat: add fallback text while data fetching

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -14,16 +15,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
+    this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((resData) => resData.places))
       .subscribe({
-        next: (resData) => {
-          console.log(resData);
-          this.places.set(resData.places);
+        next: (places) => {
+          console.log(places);
+          this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
         },
       });
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
