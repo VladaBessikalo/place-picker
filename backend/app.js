a1551d20c9efd7bd5f273f9d72ccf21260c9b143
VladaBessikalo <bess.vlada@gmail.com>
2025-09-18 16:37:19 +0200

    fix: return server response 200

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.get("/places", async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  return res.status(500).json(); // emulate a server error
+  // return res.status(500).json(); // emulate a server error
 
   const fileContent = await fs.readFile("./data/places.json");
 
--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -24,18 +24,26 @@ export class AvailablePlacesComponent implements OnInit {
     this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) =>
+          throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please tru again later. Error: ' +
+                  error.message
+              )
+          )
+        )
+      )
       .subscribe({
         next: (places) => {
           console.log(places);
           this.places.set(places);
         },
-        error: (error) => {
+        error: (error: Error) => {
           console.log(error);
-          this.error.set(
-            'Something went wrong fetching the available places. Please tru again later. Error: ' +
-              error.message
-          );
+          this.error.set(error.message);
         },
         complete: () => {
           this.isFetching.set(false);
