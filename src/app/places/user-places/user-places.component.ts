e68cd7f1ed97d6f1d839c03c0f946ff6a41343bb
VladaBessikalo <bess.vlada@gmail.com>
2025-09-19 13:43:24 +0200

    refactor: move logic to the service

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -13,19 +13,15 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
   private destroyRef = inject(DestroyRef);
   private placesService = inject(PlacesService);
+  places = this.placesService.loadUserPlaces;
 
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (places) => {
-        console.log(places);
-        this.places.set(places);
-      },
       error: (error: Error) => {
         console.log(error);
         this.error.set(error.message);
